ce1f43204bd2808a51b0af7c403d7c69c06fd83c
Jacob Hamblin <jacob@hamblin.cc>
2018-12-02 02:00:10 -0700

    provided tests part 1

--- 2018/02/tests/test.py
+++ 2018/02/tests/test.py
@@ -9,9 +9,26 @@ def expect_equal(first, second, message = 'Expected %s to be %s'):
 
 
 def test_provided_examples():
-    examples = []
+    strings = [
+        'abcdef',
+        'bababc',
+        'abbcde',
+        'abcccd',
+        'aabbcd',
+        'abcdee',
+        'ababab'
+    ]
+    examples = [
+        [strings[0], [0,0]],
+        [strings[1], [1,1]],
+        [strings[2], [1,0]],
+        [strings[3], [0,1]],
+        [strings[4], [1,0]],
+        [strings[5], [1,0]],
+        [strings[6], [0,1]],
+    ]
     for example in examples:
-        print(example)
+        expect_equal(main.has_pair_and_triplet(example[0]), example[1])
 
 
 def test_provided_part_two():
