a19e5daa4e4ff9a93778ff79ac6be19cd334d281
Jacob Hamblin <jacob@hamblin.cc>
2018-12-04 01:12:36 -0700

    prep data tests, example data

--- 2018/04/lib/main.py
+++ 2018/04/lib/main.py
@@ -1,4 +1,48 @@
+import re
 import sys
+import datetime
+
+
+def move_to_midnight(date_time):
+    if date_time.hour == 23:
+        date_time += datetime.timedelta(minutes=(60-date_time.minute))
+    return date_time
+
+
+def prep_input(input_list):
+    sort_func = lambda str: datetime.datetime.strptime(
+        str[0:18],
+        '[%Y-%m-%d %H:%M]'
+    )
+    input_list.sort(key=sort_func)
+    date_log = {}
+    for str in input_list:
+        date_time = datetime.datetime.strptime(str[0:18], '[%Y-%m-%d %H:%M]')
+        date_time = move_to_midnight(date_time)
+        guard_id = None
+        guard_id_matches = re.findall(r'#(\d+)\s', str)
+        if len(guard_id_matches):
+            guard_id = guard_id_matches[0]
+        day = '%s-%s' % (date_time.month, date_time.day)
+        entry = date_log.get(day, {'entries': []})
+        if guard_id:
+            entry['guard_id'] = guard_id
+        else:
+            entry['entries'].append((60 * date_time.hour) + date_time.minute)
+        date_log[day] = entry
+    return date_log
+
+
+def guard_shift_reports(input):
+    time_cards = {}
+    #  time_cards[id] = []
+    #  each array in time_cards[id] will have 60 boolean values
+    return time_cards
+
+
+def get_sleepiest_guard(time_cards):
+    #  for guard_id in time_cards:
+    return time_cards
 
 
 def main():
@@ -12,6 +56,7 @@ def main():
     f = open(sys.argv[1],"r")
     contents = f.read().strip()
     contents_list = contents.split('\n')
+    print(contents_list)
     f.close()
     #  print(func(contents_list))
     sys.exit(0)
--- 2018/04/tests/test.py
+++ 2018/04/tests/test.py
@@ -8,7 +8,41 @@ def expect_equal(first, second, message = 'Expected %s to be %s'):
         raise Exception(message % (first, second))
 
 
+def test_prep_input():
+    input_block = '[1518-11-01 00:00] Guard #10 begins shift\n'\
+        '[1518-11-01 00:05] falls asleep\n'\
+        '[1518-11-01 00:25] wakes up\n'\
+        '[1518-11-01 00:30] falls asleep\n'\
+        '[1518-11-01 00:55] wakes up\n'\
+        '[1518-11-01 23:58] Guard #99 begins shift\n'\
+        '[1518-11-02 00:40] falls asleep\n'\
+        '[1518-11-02 00:50] wakes up\n'\
+        '[1518-11-03 00:05] Guard #10 begins shift\n'\
+        '[1518-11-03 00:24] falls asleep\n'\
+        '[1518-11-03 00:29] wakes up\n'\
+        '[1518-11-04 00:02] Guard #99 begins shift\n'\
+        '[1518-11-04 00:36] falls asleep\n'\
+        '[1518-11-04 00:46] wakes up\n'\
+        '[1518-11-05 00:03] Guard #99 begins shift\n'\
+        '[1518-11-05 00:45] falls asleep\n'\
+        '[1518-11-05 00:55] wakes up'
+    prepped = main.prep_input(input_block.split('\n'))
+    
+    expect_equal(
+        sorted([key for key in prepped]),
+        ['11-1', '11-2', '11-3', '11-4', '11-5'],
+    )
+    entries = [
+        prepped[day]['entries'] for day in prepped
+    ]
+    expect_equal(
+        all(len(day_entries) % 2 == 0 for day_entries in entries),
+        True,
+    )
+
+
 def test_provided_examples():
+
     strings = [
     ]
     expect_equal(main.func(strings), 4)
